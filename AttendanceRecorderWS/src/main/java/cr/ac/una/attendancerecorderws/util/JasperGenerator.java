741a97acad73d81f009e78bf4389ae657f6cebd3
Dinattor <eugenio.wittingham.castrellon@est.una.ac.cr>
2026-09-06 23:30:54 -0600

    feat(jasperReports): add windowLogo, avatar and checkIcon (#8)

--- AttendanceRecorderWS/src/main/java/cr/ac/una/attendancerecorderws/util/JasperGenerator.java
+++ AttendanceRecorderWS/src/main/java/cr/ac/una/attendancerecorderws/util/JasperGenerator.java
@@ -5,7 +5,10 @@ import java.io.ByteArrayOutputStream;
 import java.io.InputStream;
 import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import net.sf.jasperreports.engine.DefaultJasperReportsContext;
 import net.sf.jasperreports.engine.JRException;
+import net.sf.jasperreports.engine.JRPropertiesUtil;
 import net.sf.jasperreports.engine.JasperExportManager;
 import net.sf.jasperreports.engine.JasperFillManager;
 import net.sf.jasperreports.engine.JasperPrint;
@@ -15,12 +18,21 @@ import net.sf.jasperreports.engine.util.JRLoader;
 
 public class JasperGenerator {
     
-    public static byte[] generateEmployeeInformationReport(List<EmployeeDtoWs> employeeDtosWs) throws JRException {        
+    private static final String CHECK_ICON_PATH = "/cr/ac/una/attendancerecorderws/resource/checkIcon.png", WINDOW_LOGO_PATH = "/cr/ac/una/attendancerecorderws/resource/windowLogo.jpg";
+    
+    public static byte[] generateEmployeeInformationReport(List<EmployeeDtoWs> employeeDtosWs) throws JRException {
         InputStream jasperStream = JasperGenerator.class.getResourceAsStream("/cr/ac/una/attendancerecorderws/jasper/relojUNA_Employee_Information_Report.jasper");
         JasperReport jasperReport = (JasperReport) JRLoader.loadObject(jasperStream);
 
         JRBeanCollectionDataSource dataSource = new JRBeanCollectionDataSource(employeeDtosWs);
-        JasperPrint jasperPrint = JasperFillManager.fillReport(jasperReport, new HashMap<>(), dataSource);
+        
+        Map<String, Object> parameters = new HashMap<>();
+        InputStream checkIcon = JasperGenerator.class.getResourceAsStream(CHECK_ICON_PATH);
+        InputStream windowLogo = JasperGenerator.class.getResourceAsStream(WINDOW_LOGO_PATH);
+        parameters.put("CHECK_ICON", checkIcon);
+        parameters.put("WINDOW_LOGO", windowLogo);
+        
+        JasperPrint jasperPrint = JasperFillManager.fillReport(jasperReport, parameters, dataSource);
 
         ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
         JasperExportManager.exportReportToPdfStream(jasperPrint, outputStream);
